fdf169aff504ebb6c33f900a1a255bd1540caf25
Praneeth <praneeth.koripalli@gmail.com>
2023-05-22 17:28:58 +0530

    added body-parser and a new create endpoint

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const data = require('./data');
 const app = express()
 const port = 3000
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.json(data)
