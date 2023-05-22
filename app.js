7b06acedd4fd94baf823a9c9360ff411920c3d1d
Praneeth <praneeth.koripalli@gmail.com>
2023-05-22 17:23:37 +0530

    Succesfully created a get call that returns data

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const data = require('./data');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.json(data)
 })
+
+app.post('/create', (req, res) => {
+    console.log(req.body);
+    res.send('record created succesfully')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+      id: 1,
+      name: 'Object 1',
+      description: 'Description for Object 1'
+    },
+    {
+      id: 2,
+      name: 'Object 2',
+      description: 'Description for Object 2'
+    },
+    {
+      id: 2,
+      name: 'Object 3',
+      description: 'Description for Object 2'
+    },
+    {
+      id: 2,
+      name: 'Object 4',
+      description: 'Description for Object 4'
+    },
+  ];
+  
+  module.exports = data;
