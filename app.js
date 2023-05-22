cab4da053cec7bc41463bfff3c5873f94185bc25
Praneeth <praneeth.koripalli@gmail.com>
2023-05-22 17:50:46 +0530

    new put and delete calls and create a bonus get by id

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 var bodyParser = require('body-parser')
-const data = require('./data');
+const issues = require('./data');
 const app = express()
 const port = 3000
 
@@ -9,15 +9,32 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+//Api's 
 app.get('/', (req, res) => {
-    res.json(data)
+    res.json(issues)
+})
+
+app.get('/:id', (req, res) => {
+    // res.json(data)
+    const issue= issues.find(d => d.id === parseInt(req.params.id));
+    if(!issue) res.status(404).send("no issue found with this id")
+    res.send(issue)
 })
 
 app.post('/create', (req, res) => {
     console.log(req.body);
     res.send('record created succesfully')
-  })
+})
+
+app.put('/update', (req, res) => {
+    console.log(req.body);
+    res.send('record updated succesfully')
+})
+app.delete('/delete', (req, res) => {
+    console.log(req.body?.id);
+    res.send('record deleted succesfully')
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const data = [
+const issues = [
     {
       id: 1,
       name: 'Object 1',
@@ -10,15 +10,15 @@ const data = [
       description: 'Description for Object 2'
     },
     {
-      id: 2,
+      id: 3,
       name: 'Object 3',
-      description: 'Description for Object 2'
+      description: 'Description for Object 3'
     },
     {
-      id: 2,
+      id: 4,
       name: 'Object 4',
       description: 'Description for Object 4'
     },
   ];
   
-  module.exports = data;
+  module.exports = issues;
